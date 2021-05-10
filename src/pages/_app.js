309a409593f71c1dacc6ec68a17a8edd768198a7
Nuno Cruz <cruz.nuno7@gmail.com>
2021-05-10 17:44:56 +0100

    Adds Google Analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,20 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { pageview } from '../utils/gtag';
 import '../styles.css';
 
 export default function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return <Component {...pageProps} />
 }
